3849c8fdf8a9164859dcdd8033155ba3c6d4bccf
IkhtiyorDev <ikhtiyordev@gmail.com>
2022-04-11 14:58:44 +0500

    Create project

---
+++ src/test/java/dev/ikhtiyor/springboottesting/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
